15f99a8e6dd890538ffc9032db968789b27a3523
AshishCoreProject <ashish017.gjimt@gmail.com>
2023-02-12 19:39:01 +0530

    using passport.js to add cookies and sessions v2

--- app.js
+++ app.js
@@ -61,10 +61,27 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
-//empty these
+//adding secret route
+app.get("/secrets", function(req, res){
+  if(req.isAuthenticated() ){
+    res.render("secrets");
+  }else{
+    redirect("/login");
+  }
+});
+
+//updating register route
 app.post("/register", function(req, res){
-
-
+  User.register({username : req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }else{
+        passport.authenticate('local')(req, res, function(){
+          res.redirect("/secrets");
+        });
+    }
+  });
 });
 
 //empty these
